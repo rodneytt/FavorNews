e0d1afbff902b2dc4d5927c5077c33bf318ba795
rodney zen <rodneytt@sina.com>
2011-07-26 23:15:24 +0800

    add a button

--- src/zen/rodney/favor/FavorNewsActivity.java
+++ src/zen/rodney/favor/FavorNewsActivity.java
@@ -2,6 +2,10 @@ package zen.rodney.favor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FavorNewsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,17 @@ public class FavorNewsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        find_and_modify_button();
     }
+    private void find_and_modify_button(){
+    	Button button = (Button)findViewById(R.id.button);
+    	button.setOnClickListener(button_listen);
+    }
+    private OnClickListener button_listen = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			setTitle("哎呦，button被点了一下");
+		}
+	};
 }
