33a8529f8ee2722b93a8c5d685bbb0e539a720d8
rodney zen <rodneytt@sina.com>
2011-08-01 22:00:17 +0800

    input text dialog

--- src/zen/rodney/favor/DialogActivity.java
+++ src/zen/rodney/favor/DialogActivity.java
@@ -11,6 +11,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,26 +29,37 @@ public class DialogActivity extends Activity {
 			}
 
 		});
+		((Button) findViewById(R.id.dialog_button3)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDialog(3);
+			}
+
+		});
 	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
 		case 1:
-			return buildDialg1(this);
-
+			return buildDialog1(this);
+		case 3:
+			return buildDialog3(this);
 		default:
 			break;
 		}
 		return null;
 	}
+
 	@Override
 	protected void onPrepareDialog(int id, Dialog dialog) {
-		if(id==1){
+		if (id == 1) {
 			setTitle("测试啊");
 		}
 	}
-	private Dialog buildDialg1(Context context) {
+
+	private Dialog buildDialog1(Context context) {
 		AlertDialog.Builder b = new AlertDialog.Builder(context);
 		b.setTitle("对话框");
 		b.setMessage("这是一个对话框?");
@@ -66,6 +78,24 @@ public class DialogActivity extends Activity {
 			}
 		});
 		return b.create();
+	}
 
+	private Dialog buildDialog3(Context context) {
+		LayoutInflater inflater = LayoutInflater.from(this);
+		final View textEntryView = inflater.inflate(R.layout.dialog_text, null);
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setTitle("请输入用户名和密码");
+		builder.setView(textEntryView);
+		builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface arg0, int arg1) {
+				setTitle("点了确定");
+			}
+		});
+		builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				setTitle("点了取消");
+			}
+		});
+		return builder.create();
 	}
 }
