140c790f968272488a08bfa511c203ae5d857cd6
rodney zen <rodneytt@sina.com>
2011-08-06 18:20:56 +0800

    dbsqlite demo

--- src/zen/rodney/favor/FavorNewsActivity.java
+++ src/zen/rodney/favor/FavorNewsActivity.java
@@ -14,7 +14,6 @@ public class FavorNewsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		((Button) findViewById(R.id.button1)).setOnClickListener(button1_listen);
-		((Button) findViewById(R.id.button2)).setOnClickListener(button2_listen);
 	}
 
 	private OnClickListener button1_listen = new OnClickListener() {
@@ -23,17 +22,7 @@ public class FavorNewsActivity extends Activity {
 		public void onClick(View paramView) {
 			System.out.println(paramView.getId());
 			Intent it = new Intent();
-			it.setClass(FavorNewsActivity.this, SharedActivity.class);
-			startActivity(it);
-		}
-	};
-	private OnClickListener button2_listen = new OnClickListener() {
-
-		@Override
-		public void onClick(View paramView) {
-			System.out.println(paramView.getId());
-			Intent it = new Intent();
-			it.setClass(FavorNewsActivity.this, ActivityList2.class);
+			it.setClass(FavorNewsActivity.this, DBSQLActivity.class);
 			startActivity(it);
 		}
 	};
